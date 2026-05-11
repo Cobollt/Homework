2f21ab159d313b2a9861527a5370032d15e3e766
Cobollt <alexgridserver@gmail.com>
2026-05-11 13:44:38 +0300

    Task

---
+++ Homework_6_Task_1.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from os import name
+
+
+def total_salsry(path):
+    total = 0
+    count = 0
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+            if not line:
+                continue
+
+            name, salary = line.split(",")
+            total += int(salary)
+            count += 1
+
+        average = total / count if count != 0 else 0
+
+        return  total, average
+
+    except FileNotFoundError:
+        print("File not found")
+        return 0,0
+    except ValueError:
+        print("Value error")
+        return 0,0
+
+total, average = total_salary("path/to/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
